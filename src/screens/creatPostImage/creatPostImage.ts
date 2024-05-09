bcc617e2a2b96ca13e287b298df607d622935f25
alejahurtd <mairahurtado930@gmail.com>
2024-05-08 21:28:03 -0500

    Feat create post and tweet

--- src/screens/creatPostImage/creatPostImage.ts
+++ src/screens/creatPostImage/creatPostImage.ts
@@ -1,24 +1,85 @@
-import { MenuBar } from '../../components/exportPapa';
-import styles from './creatPostImage.css';
+import { getDetailDoc, logIn } from '../../utils/firebase';
+import MenuBar from '../../components/menuBar/menuBar';
 
-class CreatPostScreen extends HTMLElement {
+const formData = {
+	image: '',
+	description: '',
+};
+
+class Post extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
 		this.render();
 	}
 
+	changeImage(e: any) {
+		formData.image = e?.target?.value;
+	}
+
+	changeDescription(e: any) {
+		formData.description = e?.target?.value;
+	}
+
+	submitForm() {
+		creatPost(formData);
+	}
+
 	render() {
-		if (this.shadowRoot) {
-			this.shadowRoot.innerHTML = '';
+		const sectionBar = this.ownerDocument.createElement('MenuBar') as MenuBar;
+		this.shadowRoot?.appendChild(sectionBar);
 
-			this.shadowRoot.innerHTML += `
-              <style>${styles}</style>
+		const section = this.ownerDocument.createElement('section');
+		this.shadowRoot?.appendChild(section);
 
-          `;
-		}
+		const creatPost = this.ownerDocument.createElement('div');
+		section.appendChild(creatPost);
+
+		const sectionTittle = this.ownerDocument.createElement('div');
+		creatPost.appendChild(section);
+
+		const tittle = this.ownerDocument.createElement('p');
+		tittle.innerText = 'Creat a new post';
+		sectionTittle.appendChild(tittle);
+
+		const share = this.ownerDocument.createElement('div');
+		sectionTittle.appendChild(share);
+
+		const shareButton = this.ownerDocument.createElement('button');
+		shareButton.innerText = 'share';
+		share.appendChild(shareButton);
+
+		const sectionCreatePost = this.ownerDocument.createElement('div');
+		section.appendChild(sectionCreatePost);
+
+		const sectionImage = this.ownerDocument.createElement('section');
+		sectionCreatePost.appendChild(sectionImage);
+
+		const image = this.ownerDocument.createElement('imput');
+		image.innerText = '+';
+		// image.type = 'file';
+		sectionImage.appendChild(image);
+
+		const description = this.ownerDocument.createElement('section');
+		sectionCreatePost.appendChild(description);
+
+		const textDescription = this.ownerDocument.createElement('imput');
+		textDescription.innerText = 'Write a description...';
+		description.appendChild(textDescription);
+
+		// 	if (signUpScreen) {
+		// 		// Mostrar la pantalla de registro
+		// 		signUpScreen.style.display = 'block';
+		// 	} else {
+		// 		console.error("Element with ID 'signup-screen' not found.");
+		// 	}
+		// });
+		// signUp.appendChild(signUpButton);
 	}
 }
 
-customElements.define('creatpost-image', CreatPostScreen);
-export default CreatPostScreen;
+customElements.define('app-login', Post);
+export default Post;
--- src/screens/creatPostTweet/creatPostTweet.ts
+++ src/screens/creatPostTweet/creatPostTweet.ts
@@ -1,21 +1,72 @@
-class CreatPostx extends HTMLElement {
+import { getDetailDoc, logIn } from '../../utils/firebase';
+import MenuBar from '../../components/menuBar/menuBar';
+
+const formData = {
+	description: '',
+};
+
+class Tweet extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
 		this.render();
 	}
 
+	changeDescription(e: any) {
+		formData.description = e?.target?.value;
+	}
+
+	submitForm() {
+		creatTweet(formData);
+	}
+
 	render() {
-		if (this.shadowRoot) {
-			this.shadowRoot.innerHTML = '';
+		const sectionBar = this.ownerDocument.createElement('MenuBar') as MenuBar;
+		this.shadowRoot?.appendChild(sectionBar);
 
-			this.shadowRoot.innerHTML += `
-              <style>$</style>
+		const section = this.ownerDocument.createElement('section');
+		this.shadowRoot?.appendChild(section);
 
-          `;
-		}
+		const creatPost = this.ownerDocument.createElement('div');
+		section.appendChild(creatPost);
+
+		const sectionTittle = this.ownerDocument.createElement('div');
+		creatPost.appendChild(section);
+
+		const tittle = this.ownerDocument.createElement('p');
+		tittle.innerText = 'Creat a new tweet';
+		sectionTittle.appendChild(tittle);
+
+		const share = this.ownerDocument.createElement('div');
+		sectionTittle.appendChild(share);
+
+		const shareButton = this.ownerDocument.createElement('button');
+		shareButton.innerText = 'share';
+		share.appendChild(shareButton);
+
+		const sectionCreateTweet = this.ownerDocument.createElement('div');
+		section.appendChild(sectionCreateTweet);
+
+		const description = this.ownerDocument.createElement('section');
+		sectionCreateTweet.appendChild(description);
+
+		const textDescription = this.ownerDocument.createElement('imput');
+		textDescription.innerText = 'Write a tweet...';
+		description.appendChild(textDescription);
+
+		// 	if (signUpScreen) {
+		// 		// Mostrar la pantalla de registro
+		// 		signUpScreen.style.display = 'block';
+		// 	} else {
+		// 		console.error("Element with ID 'signup-screen' not found.");
+		// 	}
+		// });
+		// signUp.appendChild(signUpButton);
 	}
 }
 
-customElements.define('creatpost-tweet', CreatPostx);
-export default CreatPostx;
+customElements.define('app-login', Tweet);
+export default Tweet;
